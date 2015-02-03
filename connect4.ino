f347e9fc0172a7a48c30d5967360b920f74370ba
PaulRB <paul.beard@databaseconnect.co.uk>
2015-02-03 14:36:44 +0000

    Update connect4.ino

--- connect4.ino
+++ connect4.ino
@@ -2,10 +2,7 @@
 // P Beard
 // Jan 2015
 
-// Used as array of bits for positions of "X" tokens
-unsigned long long boardO;
-// Used as array of bits for positions of "O" tokens
-unsigned long long boardX;
+char board[43];
 
 // Array containing start position of each "win line"
 byte lineStart[] = { 0,  7, 14, 21, 28, 35,  0,  1,  2,  3,  4,  5,  6, 14,  7,  0,  1,  2,  3,  3,  4,  5,  6, 13, 20};
@@ -19,7 +16,8 @@ void setup() {
   Serial.println();
   Serial.println("~~~~~~Connect 4~~~~~~");
   Serial.println();
-  displayBoard(boardO, boardX);
+  for (byte i = 0; i < 43; i++) board[i] = '.';
+  displayBoard(board);
 }
 
 void loop() {
@@ -35,12 +33,12 @@ void loop() {
   Serial.println();
   while (Serial.available() < 1) {} // wait for CR code
   Serial.read(); // discard CR code
-  dropCounter(boardX, boardO, c - '0');
-  displayBoard(boardO, boardX);
+  dropCounter(board, 'O', c - '0');
+  displayBoard(board);
   
-  if (score(boardO, boardX) < -500) {
+  if (score(board, 'X', 'O') < -500) {
     Serial.println();
-    Serial.println ("****CONGRATULATIONS YOU WIN****");
+    Serial.println ("****CONGRATULATIONS YOU WIN**** ");
     Serial.println();
     Serial.println("Press reset for new game");
     while (true) {}
@@ -49,13 +47,13 @@ void loop() {
     
     Serial.println();
     Serial.print("My go:");
-    currentScore = bestMove(boardO, boardX, best, 3);
+    currentScore = bestMove(board, 'X', 'O', best, 4);
     Serial.println(best);
     Serial.println();
-    dropCounter(boardO, boardX, best);
-    displayBoard(boardO, boardX);
+    dropCounter(board, 'X', best);
+    displayBoard(board);
     
-    if (score(boardO, boardX) > 500) {
+    if (score(board, 'X', 'O') > 500) {
       Serial.println();
       Serial.println ("****COMISERATIONS YOU LOST****");
       Serial.println();
@@ -65,61 +63,53 @@ void loop() {
   }
 }
 
-void displayBoard(unsigned long long boardO, unsigned long long boardX) {
+void displayBoard(char board[]) {
   
-  unsigned long long mask = 1ULL;
+  Serial.println("0123456");
   
-  for(byte col = 0; col <= 6; col++) Serial.print(col);
-  Serial.println();
+  byte i = 0;
   
   for(byte row = 0; row <= 5; row++) {
     for(byte col = 0; col <= 6; col++) {
-      if (boardO & mask) Serial.print('O');
-      else if (boardX & mask) Serial.print('X');
-      else Serial.print('.');
-      mask = mask << 1;
+      Serial.print(board[i++]);
     }
     Serial.println();
   }
 }
 
-void dropCounter(unsigned long long &boardO, unsigned long long &boardX, byte col) {
+void dropCounter(char board[], char token, byte col) {
   
-  unsigned long long mask = 1ULL << col;
-  byte row = 0;
+  byte i = col;
   
-  while (row < 5) {
-    // Test if the position one row down is empty
-    unsigned long long mask2 = mask << 7;
-    if (((boardO | boardX) & mask2)) break;
+  // Test if the position one row down is empty
+  while (i < 35 && board[i+7] == '.') {
     // Move down a row
-    row++;
-    mask = mask2;
+    i += 7;
   }
   // Place the token here
-  boardO |= mask;
+  board[i] = token;
 }
 
-int score(unsigned long long &boardO, unsigned long long &boardX) {
+int score(char board[], char token, char oppToken) {
   
   int totalScore = 0;
   
   // Scan each "win" line on the board
   for (byte line = 0; line < 25; line++) {
     
-    unsigned long long mask = 1ULL << lineStart[line];
+    byte i = lineStart[line];
     byte lineStep = lineSteps[line];
   
     // Scan each group of 4 positions on the "win" line
     for (byte rep = 0; rep < lineReps[line]; rep++) {
       
-      unsigned long long mask2 = mask;
+      byte j = i;
       int addScore = 0;
       
       // Check this group of 4 positions
       for (byte pos = 0; pos < 4; pos++) {
         
-        if (boardO & mask2) {
+        if (board[j] == token) {
           if (addScore < 0) {
             // A mix of "O" and "X" - no-one can win here
             addScore = 0;
@@ -131,7 +121,7 @@ int score(unsigned long long &boardO, unsigned long long &boardX) {
           else if (addScore == 100) addScore = 1000;
         }
   
-        if (boardX & mask2) {
+        if (board[j] == oppToken) {
           if (addScore > 0) {
             // A mix of "O" and "X" - no-one can win here
             addScore = 0;
@@ -143,19 +133,18 @@ int score(unsigned long long &boardO, unsigned long long &boardX) {
           else if (addScore == -100) addScore = -1000;
         }
         // Move to next position in group of 4
-        mask2 = mask2 << lineStep;
+        j += lineStep;
       }
       // Move to next start position of next group-of-4 on the win line
-      mask = mask << lineStep;
+      i += lineStep;
       // Add any score to the total score for the board
       totalScore += addScore;
     }
   }
- 
   return totalScore;
 }
 
-int bestMove(unsigned long long &boardO, unsigned long long &boardX, byte &best, byte lookAhead) {
+int bestMove(char board[], char token, char oppToken, byte &best, byte lookAhead) {
   
   int bestScore = -9999;
   byte best2;
@@ -163,19 +152,22 @@ int bestMove(unsigned long long &boardO, unsigned long long &boardX, byte &best,
   // Evaluate each choice of column
   for (byte col = 0; col < 7; col++) {
     
-    // Make a copy of the board
-    unsigned long long boardOcopy = boardO;
-    unsigned long long boardXcopy = boardX;
     int thisScore;
+    
+    // Make a copy of the board
+    char boardCopy[43];
+    for (byte i = 0; i < 43; i++) boardCopy[i] = board[i];
+
     // Drop token onto the copy of the board    
-    dropCounter(boardOcopy, boardXcopy, col);
+    dropCounter(boardCopy, token, col);
     
     if (lookAhead == 0) 
       // Don't look any more moves ahead, just evaluate the board as-is
-      thisScore = score(boardOcopy, boardXcopy);
-    else
+      thisScore = score(boardCopy, token, oppToken);
+    else {
       // Look more moves ahead, but swap places with opponent since it will be their turn next
-      thisScore = -bestMove(boardXcopy, boardOcopy, best2, lookAhead - 1);
+      thisScore = -bestMove(boardCopy, oppToken, token, best2, lookAhead - 1);
+    }
       
     // Check this choice of column - best so far?
     if (thisScore > bestScore) {
@@ -186,4 +178,5 @@ int bestMove(unsigned long long &boardO, unsigned long long &boardX, byte &best,
   }
   return bestScore;
 }
+
 
