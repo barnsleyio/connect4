ade27ef3a756040464a69b6ffc61d25e9d62a2b2
PaulRB <paul.beard@databaseconnect.co.uk>
2015-05-30 19:39:54 +0100

    Update connect4.ino
    Added switch matrix control

--- connect4.ino
+++ connect4.ino
@@ -2,7 +2,25 @@
 // P Beard
 // Jan 2015
 
-char board[43];
+#include "U8glib.h"
+
+#define LED 13
+#define NO_BUTTONS 0b11111111111111
+
+const byte btnRow[2] = {2, 3};
+const byte btnCol[7] = {4, 5, 6, 7, 8, 9, 10};
+
+unsigned int btns = NO_BUTTONS;
+unsigned int lastBtns = NO_BUTTONS;
+unsigned int stableBtns = NO_BUTTONS;
+unsigned long debounce;
+char human = '-';
+char robot = '-';
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST);	// Fast I2C / TWI 
+
+char board[42];
+char screen[7][22];
 
 // Array containing start position of each "win line"
 byte lineStart[] = { 0,  7, 14, 21, 28, 35,  0,  1,  2,  3,  4,  5,  6, 14,  7,  0,  1,  2,  3,  3,  4,  5,  6, 13, 20};
@@ -12,56 +30,125 @@ byte lineReps[] =  { 4,  4,  4,  4,  4,  4,  3,  3,  3,  3,  3,  3,  3,  1,  2, 
 byte lineSteps[] = { 1,  1,  1,  1,  1,  1,  7,  7,  7,  7,  7,  7,  7,  8,  8,  8,  8,  8,  8,  6,  6,  6,  6,  6,  6};
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println();
-  Serial.println("~~~~~~Connect 4~~~~~~");
-  Serial.println();
-  for (byte i = 0; i < 43; i++) board[i] = '.';
+  pinMode(LED, OUTPUT);
+  //Serial.begin(9600);
+  u8g.setFont(u8g_font_6x10);
+  u8g.setFontPosTop();
+  for (byte i = 0; i < 42; i++) board[i] = '.';
+  for (byte i = 0; i < 7; i++) {
+    for (byte j = 0; j < 21; j++) {
+      screen[i][j] = '\0';
+    }
+  }
+  strcpy(screen[0], "Your go:");
   displayBoard(board);
+  updateScreen();
+
+  pinMode(btnRow[0], INPUT_PULLUP);
+  pinMode(btnRow[1], INPUT_PULLUP);
+  
+  for (byte col=0; col<=6; col++) {
+    pinMode(btnCol[col], OUTPUT);
+    digitalWrite(btnCol[col], LOW);
+  }
 }
 
 void loop() {
+
   char c;
   byte best;
   int currentScore;
+  unsigned int btnsNow = 0;
   
-  Serial.println();
-  Serial.print("Your go:");
-  while (Serial.available() < 1) {} // wait for key code
-  c = Serial.read();
-  Serial.println(c);
-  Serial.println();
-  while (Serial.available() < 1) {} // wait for CR code
-  Serial.read(); // discard CR code
-  dropCounter(board, 'O', c - '0');
-  displayBoard(board);
-  
-  if (score(board, 'X', 'O') == -1000) {
-    Serial.println();
-    Serial.println ("****CONGRATULATIONS YOU WIN**** ");
-    Serial.println();
-    Serial.println("Press reset for new game");
-    while (true) {}
-  }
-  else {
-    
-    Serial.println();
-    Serial.print("My go:");
-    //unsigned long t = millis();
-    currentScore = bestMove(board, 'X', 'O', best, 4);
-    //Serial.println(millis() - t);
-    Serial.println(best);
-    Serial.println();
-    dropCounter(board, 'X', best);
-    displayBoard(board);
-    
-    if (score(board, 'X', 'O') == 1000) {
-      Serial.println();
-      Serial.println ("****COMISERATIONS YOU LOST****");
-      Serial.println();
-      Serial.println("Press reset for new game");
-      while (true) {}
+  // Read all buttons in the 7 cols x 2 rows matrix
+  for (byte row=0; row<=1; row++) {
+    for (byte col=0; col<=6; col++) {
+      pinMode(btnCol[col], INPUT);
+      btnsNow = btnsNow << 1 | digitalRead(btnRow[row]);
+      pinMode(btnCol[col], OUTPUT);
+      digitalWrite(btnCol[col], LOW);
     }
+  }
+  
+  // Has there been any change in the buttons?
+  if (btnsNow != lastBtns) {
+    // Note the new button values and the time of the change
+    lastBtns = btnsNow;
+    debounce = millis();
+  }
+  
+  // Do we have a new, stable reading?
+  if ((millis() - debounce) > 50 && btnsNow != stableBtns) {
+    stableBtns = btnsNow;
+    btns &= btnsNow;
+  }
+  
+  // Have we detected some button presses and the timeout period for further changes has expired?
+  if ((millis() - debounce) > 500 && btns != NO_BUTTONS) {
+    // Work out which column the counter was placed in. Zero indicates cheating! 
+    unsigned int col = btns & btns >> 7;
+    if (col == 0) {
+      strcpy(screen[0], "Cheat!");
+      updateScreen();
+    }
+    else {
+
+      // Figure out which colour counter has been dropped
+      char counter;
+      if ((btns & 0b1111111) == col) counter = 'O'; else counter = 'X';
+
+      // Has human chosen a colour yet?
+      if (human == '-') {
+        // Human has now chosen a colour and robot chooses the other
+        human = counter;
+        if (human == 'X') robot = 'O'; else robot = 'X';
+      }
+
+      // Has human played with robot's colour?
+      if (counter != human) {
+        strcpy(screen[0], "Cheat!");
+        updateScreen();
+      }
+      else {
+        // Human has played correct colour. Figure out which column
+        for (byte i=0; i<=6; i++) {
+          if (bit(i) == col) {
+            c = 6-i;
+          }
+        }
+        // Drop the counter
+        dropCounter(board, human, c);
+        displayBoard(board);
+        updateScreen();
+        
+        if (score(board, human, robot) == 1000) {
+          strcpy(screen[0], "You Won!");
+          updateScreen();
+          while (true) {}
+        }
+        else {
+          
+          //unsigned long t = millis();
+          strcpy(screen[0], "My go:");
+          updateScreen();
+          currentScore = bestMove(board, robot, human, best, 4);
+          digitalWrite(LED, LOW);
+          //Serial.println(millis() - t);
+          dropCounter(board, robot, best);
+          displayBoard(board);
+          
+          if (score(board, robot, human) == 1000) {
+            strcpy(screen[0], "You Lost!");
+            updateScreen();
+            while (true) {}
+          }
+          
+          strcpy(screen[0], "Your go:");
+          updateScreen();
+        }
+      }        
+    }
+    btns = NO_BUTTONS;
   }
 }
 
@@ -73,10 +160,25 @@ void displayBoard(char board[]) {
   
   for(byte row = 0; row <= 5; row++) {
     for(byte col = 0; col <= 6; col++) {
-      Serial.print(board[i++]);
+      screen[row+1][col] = board[i++];
     }
-    Serial.println();
   }
+  updateScreen();
+}
+
+void updateScreen() {
+  // U8GLIB picture loop
+  u8g.firstPage();  
+  do {
+    uint8_t i, y;
+    
+    y = 0;       // reference is the top left -1 position of the string
+    y--;           // correct the -1 position of the drawStr 
+    for( i = 0; i < 7; i++ ) {
+      u8g.drawStr( 0, y, (char *)(screen[i]));
+      y += u8g.getFontLineSpacing();
+    }
+  } while( u8g.nextPage() );
 }
 
 byte dropCounter(char board[], char token, byte col) {
@@ -163,6 +265,8 @@ int bestMove(char board[], char token, char oppToken, byte &bestCol, byte lookAh
   char boardCopy[43];
   for (byte i = 0; i < 43; i++) boardCopy[i] = board[i];
   
+  if (lookAhead > 1) digitalWrite(LED, !digitalRead(LED));
+  
   // Evaluate each choice of column
   for (byte col = 0; col < 7; col++) {
     if (board[col] == '.') {
@@ -185,12 +289,13 @@ int bestMove(char board[], char token, char oppToken, byte &bestCol, byte lookAh
       }
       // remove the counter again
       boardCopy[counterPosition] = '.';
+      
+      // check for any button presses = cheating
+      if (digitalRead(btnRow[0]) == LOW || digitalRead(btnRow[1]) == LOW) {
+        strcpy(screen[0], "Cheat!");
+        updateScreen();
+      }
     }
   }
   return bestScore;
 }
-
-
-
-
-
